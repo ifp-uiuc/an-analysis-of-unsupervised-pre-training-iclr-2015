80e20e82f552fba22533985c5e73b011856c7f81
Tom Paine <tom.le.paine@gmail.com>
2015-01-16 17:12:04 -0600

    Updated model files.

--- stl10/adu_model.py
+++ stl10/adu_model.py
@@ -4,9 +4,9 @@ from anna.layers import layers, cc_layers
 import anna.models
 
 
-class UnsupervisedModel(fastor.models.UnsupervisedModel):
+class UnsupervisedModel(anna.models.UnsupervisedModel):
     batch = 128
-    input = cc_layers.CudaConvnetInput2DLayer(batch, 3, 96, 96)
+    input = cc_layers.Input2DLayer(batch, 3, 96, 96)
 
     k = float(numpy.random.rand()*1+0.2)
     print '## k = %.3f' % k
@@ -20,42 +20,42 @@ class UnsupervisedModel(fastor.models.UnsupervisedModel):
 
     nonlinearity = trec
 
-    conv1 = cc_layers.CudaConvnetConv2DNoBiasLayer(
+    conv1 = cc_layers.Conv2DNoBiasLayer(
         input,
         n_filters=64,
         filter_size=5,
         weights_std=winit1,
         nonlinearity=nonlinearity,
         pad=2)
-    pool1 = cc_layers.CudaConvnetPooling2DLayer(conv1, 2, stride=2)
-    conv2 = cc_layers.CudaConvnetConv2DNoBiasLayer(
+    pool1 = cc_layers.Pooling2DLayer(conv1, 2, stride=2)
+    conv2 = cc_layers.Conv2DNoBiasLayer(
         pool1,
         n_filters=128,
         filter_size=5,
         weights_std=winit2,
         nonlinearity=nonlinearity,
         pad=2)
-    pool2 = cc_layers.CudaConvnetPooling2DLayer(conv2, 2, stride=2)
-    conv3 = cc_layers.CudaConvnetConv2DNoBiasLayer(
+    pool2 = cc_layers.Pooling2DLayer(conv2, 2, stride=2)
+    conv3 = cc_layers.Conv2DNoBiasLayer(
         pool2,
         n_filters=256,
         filter_size=5,
         weights_std=winit3,
         nonlinearity=nonlinearity,
         pad=2)
-    deconv3 = cc_layers.CudaConvnetDeconv2DNoBiasLayer(
+    deconv3 = cc_layers.Deconv2DNoBiasLayer(
         conv3, conv3, nonlinearity=layers.identity)
-    unpool4 = cc_layers.CudaConvnetUnpooling2DLayer(deconv3, pool2)
-    deconv4 = cc_layers.CudaConvnetDeconv2DNoBiasLayer(
+    unpool4 = cc_layers.Unpooling2DLayer(deconv3, pool2)
+    deconv4 = cc_layers.Deconv2DNoBiasLayer(
         unpool4, conv2, nonlinearity=layers.identity)
-    unpool5 = cc_layers.CudaConvnetUnpooling2DLayer(deconv4, pool1)
-    output = cc_layers.CudaConvnetDeconv2DNoBiasLayer(
+    unpool5 = cc_layers.Unpooling2DLayer(deconv4, pool1)
+    output = cc_layers.Deconv2DNoBiasLayer(
         unpool5, conv1, nonlinearity=layers.identity)
 
 
-class SupervisedModel(fastor.models.SupervisedModel):
+class SupervisedModel(anna.models.SupervisedModel):
     batch = 128
-    input = cc_layers.CudaConvnetInput2DLayer(batch, 3, 96, 96)
+    input = cc_layers.Input2DLayer(batch, 3, 96, 96)
 
     k = float(numpy.random.rand()*1+0.2)
     print '## k = %.3f' % k
@@ -69,30 +69,30 @@ class SupervisedModel(fastor.models.SupervisedModel):
 
     nonlinearity = trec
 
-    conv1 = cc_layers.CudaConvnetConv2DNoBiasLayer(
+    conv1 = cc_layers.Conv2DNoBiasLayer(
         input,
         n_filters=64,
         filter_size=5,
         weights_std=winit1,
         nonlinearity=nonlinearity,
         pad=2)
-    pool1 = cc_layers.CudaConvnetPooling2DLayer(conv1, 2, stride=2)
-    conv2 = cc_layers.CudaConvnetConv2DNoBiasLayer(
+    pool1 = cc_layers.Pooling2DLayer(conv1, 2, stride=2)
+    conv2 = cc_layers.Conv2DNoBiasLayer(
         pool1,
         n_filters=128,
         filter_size=5,
         weights_std=winit2,
         nonlinearity=nonlinearity,
         pad=2)
-    pool2 = cc_layers.CudaConvnetPooling2DLayer(conv2, 2, stride=2)
-    conv3 = cc_layers.CudaConvnetConv2DNoBiasLayer(
+    pool2 = cc_layers.Pooling2DLayer(conv2, 2, stride=2)
+    conv3 = cc_layers.Conv2DNoBiasLayer(
         pool2,
         n_filters=256,
         filter_size=5,
         weights_std=winit3,
         nonlinearity=nonlinearity,
         pad=2)
-    pool3 = cc_layers.CudaConvnetPooling2DLayer(conv3, 12, stride=12)
+    pool3 = cc_layers.Pooling2DLayer(conv3, 12, stride=12)
 
     winitD1 = k/numpy.sqrt(numpy.prod(pool3.get_output_shape()))
     winitD2 = k/numpy.sqrt(512)
