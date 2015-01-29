982e408d9c77e0e79e9027c724ca4184ab73617c
Pooya Khorrami <pkhorrami4@gmail.com>
2015-01-29 10:54:32 -0600

    Modified data loading mechanism in reduced 5_to_1 cifar10 experiments and test them.

--- cifar10/5_to_1/cnn_u/train.py
+++ cifar10/5_to_1/cnn_u/train.py
@@ -25,26 +25,28 @@ monitor = util.Monitor(model)
 
 # Loading CIFAR-10 dataset
 print('Loading Data')
-train_iterator = util.get_cifar_iterator_reduced('train',
-                                                 mode='random_uniform',
-                                                 batch_size=128,
-                                                 num_batches=100000,
-                                                 rescale=True,
-                                                 num_samples_per_class=1000,
-                                                 which_split=0)
+data_path = '/data/cifar10/'
+reduced_data_path = os.path.join(data_path, 'reduced', 'cifar10_1000')
 
-test_iterator = util.get_cifar_iterator('test',
-                                        mode='random_uniform',
-                                        batch_size=128,
-                                        num_batches=100000,
-                                        rescale=True)
+train_data = numpy.load(os.path.join(reduced_data_path, 'train_X_split_0.npy'))
+train_labels = numpy.load(os.path.join(reduced_data_path,
+                          'train_y_split_0.npy'))
+test_data = numpy.load('/data/cifar10/test_X.npy')
+test_labels = numpy.load('/data/cifar10/test_y.npy')
+
+train_dataset = supervised_dataset.SupervisedDataset(train_data, train_labels)
+test_dataset = supervised_dataset.SupervisedDataset(test_data, test_labels)
+train_iterator = train_dataset.iterator(
+    mode='random_uniform', batch_size=128, num_batches=100000)
+test_iterator = test_dataset.iterator(mode='sequential', batch_size=128)
 
 normer = util.Normer2(filter_size=5, num_channels=3)
 
 print('Training Model')
 for x_batch, y_batch in train_iterator:
+    x_batch = x_batch.transpose(1, 2, 3, 0)
     x_batch = normer.run(x_batch)
-    y_batch = numpy.int64(numpy.argmax(y_batch, axis=1))
+    #y_batch = numpy.int64(numpy.argmax(y_batch, axis=1))
     monitor.start()
     log_prob, accuracy = model.train(x_batch, y_batch)
     monitor.stop(1-accuracy)  # monitor takes error instead of accuracy
@@ -52,7 +54,8 @@ for x_batch, y_batch in train_iterator:
     if monitor.test:
         monitor.start()
         x_test_batch, y_test_batch = test_iterator.next()
+        x_test_batch = x_test_batch.transpose(1, 2, 3, 0)
         x_test_batch = normer.run(x_test_batch)
-        y_test_batch = numpy.int64(numpy.argmax(y_test_batch, axis=1))
+        #y_test_batch = numpy.int64(numpy.argmax(y_test_batch, axis=1))
         test_accuracy = model.eval(x_test_batch, y_test_batch)
         monitor.stop_test(1-test_accuracy)
